bc9925babf4488a4c86dc42e3d13391fe5719d69
Busirat Bolanle AZEEZ <azeez.busirat@gmail.com>
2022-06-19 14:59:24 +0100

    models

--- I4G0003711KY/blog/models.py
+++ I4G0003711KY/blog/models.py
@@ -1,4 +1,5 @@
 from tkinter import CASCADE
+from typing import Any
 from typing_extensions import Self
 from django.db import models
 from typing_extensions import Self
@@ -8,7 +9,7 @@ User = get_user_model()
 class Post (models.Model):
     Title = models.CharField(max_length=200)
     Text = models.TextField()
-    Author = models.ForeignKey(User, on_delete=CASCADE)
+    Author = models.ForeignKey(User, on_delete=models.CASCADE)
     Created_date = models.DateTimeField('created_date')
     Published_date = models.DateTimeField('published_date')
     def __str__(self) -> str:
