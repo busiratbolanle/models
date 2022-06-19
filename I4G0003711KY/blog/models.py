a6bf0ec560787d03ae892f7449f3b77c6d825ada
Busirat Bolanle AZEEZ <azeez.busirat@gmail.com>
2022-06-19 14:40:33 +0100

    models

---
+++ I4G0003711KY/blog/models.py
@@ -0,0 +1,17 @@
+from tkinter import CASCADE
+from typing_extensions import Self
+from django.db import models
+from typing_extensions import Self
+from django.contrib.auth import get_user_model
+User = get_user_model()
+# Create your models here.
+class Post (models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField()
+    Author = models.ForeignKey(User, on_delete=CASCADE)
+    Created_date = models.DateTimeField('created_date')
+    Published_date = models.DateTimeField('published_date')
+    def __str__(self) -> str:
+        return super().__str__()
+    
+    
